a115dac6bc901a66eece60c9330a0a62b9f666e8
barcolana <emilyman@hotmail.it>
2026-02-20 17:05:54 +0100

    Fix Swoosh config

--- config/config.exs
+++ config/config.exs
@@ -67,4 +67,4 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
-config :swoosh, :api_client, Swoosh.ApiClient.Finch
+config :swoosh, api_client: Swoosh.ApiClient.Finch
